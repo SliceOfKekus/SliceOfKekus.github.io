6ee13fee0536cd4dbc34f1ae3c3ef1e6b61d0bc2
Dmitry <sliceofkekus@icloud.com>
2021-11-16 19:12:33 +0300

    test #4

--- js/header/menu_button__hamburger.js
+++ js/header/menu_button__hamburger.js
@@ -1,11 +1,14 @@
 const menuButton = document.querySelector('.menu_button');
+const navigationMenu = document.querySelector('.navigation_menu');
 let isOpen = false;
 menuButton.addEventListener('click', () => {
     if (!isOpen) {
         menuButton.classList.add('open');
+        //navigationMenu.classList.add('active');
         isOpen = true;
     } else {
         menuButton.classList.remove('open');
+        //navigationMenu.classList.remove('active');
         isOpen = false;
     }
 });
