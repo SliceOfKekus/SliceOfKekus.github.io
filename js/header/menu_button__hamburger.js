81007ad505d6f36fa4f6532a65d2aec862f7784c
Dmitry <sliceofkekus@icloud.com>
2021-11-17 00:32:22 +0300

    sizing test №... idk =(

--- js/header/menu_button__hamburger.js
+++ js/header/menu_button__hamburger.js
@@ -1,12 +1,16 @@
 const menuButton = document.querySelector('.menu_button');
 const navigationMenu = document.querySelector('.navigation_menu');
 let isOpen = false;
+
+
 menuButton.addEventListener('click', () => {
-    if (!isOpen) {
+    menuButton.classList.toggle("open");
+    navigationMenu.classList.toggle("open");
+    /*if (!isOpen) {
         menuButton.classList.add('open');
         isOpen = true;
     } else {
         menuButton.classList.remove('open');
         isOpen = false;
-    }
+    }*/
 });
