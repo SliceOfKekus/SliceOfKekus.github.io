0ff7270faca26596a122f782c82080702a553cec
Dmitry <sliceofkekus@icloud.com>
2021-11-28 10:00:10 +0300

    index page testing

--- js/header/menu_button__hamburger.js
+++ js/header/menu_button__hamburger.js
@@ -1,16 +1,7 @@
 const menuButton = document.querySelector('.menu_button');
 const navigationMenu = document.querySelector('.navigation_menu');
-let isOpen = false;
-
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle("open");
     navigationMenu.classList.toggle("open");
-    /*if (!isOpen) {
-        menuButton.classList.add('open');
-        isOpen = true;
-    } else {
-        menuButton.classList.remove('open');
-        isOpen = false;
-    }*/
 });
